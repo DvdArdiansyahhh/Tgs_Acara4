4c7cbd82e7e7cd9d9b2f3f6d631ad366a8ab2ef3
DvdArdiansyahhh <124945304+DvdArdiansyahhh@users.noreply.github.com>
2024-02-16 13:26:22 +0700

    Acara 5

---
+++ acara5.dart
@@ -0,0 +1,38 @@
+// void main() {
+//   for (int i = 0; i < 5; i++) {
+//     print('hello ${i + 1}');
+//   }
+// }
+
+// void main() { 
+//  var sentences = "dart"; 
+//  print(sentences[0]); // "d"
+//  print(sentences[2]);// "r"
+// }
+
+// void main() {
+//  // declare an integer
+//  int num1 = 10; 
+ 
+//  // declare a double value
+//  double num2 = 10.50; 
+//  // print the values
+//  print(num1); //10
+//  print(num2); //10.5
+// }
+
+// void main() { 
+//  print(num.parse('12')); //12
+//  print(num.parse('10.91')); //10.91
+// }
+
+// void main() { 
+//  print(num.parse('12A')); 
+//  print(num.parse('AAAA')); 
+// }
+
+void main() { 
+ int j = 45;
+ String t = "$j"; 
+ print("hello"+ t);
+}
