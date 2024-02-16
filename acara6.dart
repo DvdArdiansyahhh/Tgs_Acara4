a70d581f778e92eb2dfbafdee0692f68b7b5d003
DvdArdiansyahhh <124945304+DvdArdiansyahhh@users.noreply.github.com>
2024-02-16 14:55:13 +0700

    Acara 6

---
+++ acara6.dart
@@ -0,0 +1,89 @@
+// void main(){
+//  var isThisWahyu = true;
+ 
+//  if(isThisWahyu){
+//  print("wahyu");
+//  }else{
+//  print("bukan");
+//  }
+// }
+
+// void main(){
+//  var isThisWahyu = true;
+//  isThisWahyu ? print("wahyu") : print("bukan");
+//  }
+
+// Contoh 1
+// void main() { 
+//  if ( true) {
+//  print("jalankan code");
+//  }
+// }
+
+// Contoh 2
+// void main() { 
+//  if ( false ) {
+//  print("Program tidak jalan code");
+//  }
+// }
+
+// Contoh 3
+// void main() { 
+// var mood = "happy";
+//  if ( mood == "happy" ) {
+//  print("hari ini aku bahagia!");
+//  }
+// }
+
+// Contoh 4
+// void main() {
+//  var minimarketStatus = "open";
+//  if (minimarketStatus == "open") {
+//  print("saya akan membeli telur dan buah");
+//  } else {
+//  print("minimarketnya tutup");
+//  }
+// }
+
+// Contoh 5
+// void main() {
+//  var minimarketStatus = "close";
+//  var minuteRemainingToOpen = 5;
+//  if (minimarketStatus == "open") {
+//  print("saya akan membeli telur dan buah");
+//  } else if (minuteRemainingToOpen <= 5) {
+//  print("minimarket buka sebentar lagi, saya tungguin");
+//  } else {
+//  print("minimarket tutup, saya pulang lagi");
+//  }
+// }
+
+// Contoh 6
+// void main() {
+//  var minimarketStatus = "open";
+//  var telur = "soldout";
+//  var buah = "soldout";
+//  if (minimarketStatus == "open") {
+//  print("saya akan membeli telur dan buah");
+//  if (telur == "soldout" || buah == "soldout") {
+//  print("belanjaan saya tidak lengkap");
+//  } else if (telur == "soldout") {
+//  print("telur habis");
+//  } else if (buah == "soldout") {
+//  print("buah habis");
+//  }
+//  } else {
+//  print("minimarket tutup, saya pulang lagi");
+//  }
+// }
+
+// // Contoh7
+// void main() { 
+// var buttonPushed = 1;
+// switch(buttonPushed) {
+//  case 1: { print('matikan TV!'); break; }
+//  case 2: { print('turunkan volume TV!'); break; }
+//  case 3: { print('tingkatkan volume TV!'); break; }
+//  case 4: { print('matikan suara TV!'); break; }
+//  default: { print('Tidak terjadi apa-apa'); }}
+// }
